f0880a9bca3d4f388326f19a2fa3ac7ca83b6f9f
nithi-p <nprasarnphanich@gmail.com>
2015-06-29 05:51:08 -0400

    again

--- place.js
+++ place.js
@@ -89,13 +89,13 @@ function initialize() {
             google.maps.event.addListener(map, 'center_changed', function() {
                 // 0.1 seconds after the center of the map has changed,
                 // set back the marker position.
-               /*window.setTimeout(function() {*/
+               window.setTimeout(function() {
                   var center = map.getCenter();
                   //marker.setPosition(center);
                   infowindow.setPosition(center);
                   sendPlace(center.A,center.F);
                   //getmapData(center.A,center.F);
-              /* }, 1000);*/
+               }, 2000);
                 
             });
 
